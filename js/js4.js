b573fc43eca468fdadd75f844909f1bf62ae8b35
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-02 16:52:33 +0500

    arithmetic operators and comparisons

---
+++ js/js4.js
@@ -0,0 +1,26 @@
+console.log(2+2)
+console.log(10-1)
+console.log(5*10)
+console.log(100/25)
+console.log(2**10)
+console.log(13%5)
+console.log(5%2)
+console.log(5* -2 + 10 / -3)
+console.log('3' + '5')
+console.log(+'3'+ + '5')
+const result = 1+ -2 -3 * + '4' / 5 **2
+console.log(result) 
+
+let count = 1
+
+count +=2
+count -=5
+count *=10
+count /=3
+
+console.log(count)
+
+let message = 'Привет'
+message += ', друг!'
+message +=' Как дела?'
+console.log(message)
