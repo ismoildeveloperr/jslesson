2cff3e075d04bde5c1243b233656e365a502c88f
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-02 14:47:58 +0500

    first commit

---
+++ js/js1.js
@@ -0,0 +1,9 @@
+console.log('Привет', 555, 'Что то ещё')
+console.log('Ismoil')
+console.log(2323)
+// ismoil
+
+/*
+ismoil
+ismoil
+ */
---
+++ js/js2.js
@@ -0,0 +1,15 @@
+//
+// let message = 'Привет'
+// console.log(message)
+// message = 'Пока'
+// console.log(message)
+// Переменная — это имя, за которым хранится значение.
+let name = "Ali";         // строка
+let age = 30;             // число
+let isStudent = true;     // булево значение
+let skills = ["JS", "CSS"]; // массив
+
+// var — устаревшее объявление переменных
+var message = "Hello";
+// const — постоянные значения
+const birthYear = 1995;
