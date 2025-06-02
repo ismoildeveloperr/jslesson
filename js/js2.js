e202799077a575149f9bc8d26031bab9f64a379a
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-02 15:36:31 +0500

    Плюс сделаем 2 переменние

--- js/js2.js
+++ js/js2.js
@@ -17,14 +17,33 @@ const birthYear = 1995;
 
 const greeting = 'Привет'
 const names = "Александр"
-const goodbye = `пока`
+// const goodbye = `пока`
 
-const message = `${greeting}, ${names}!`
+const messages = `${greeting}, ${names}`;
 
-console.log(message)
+console.log(messages)
 
 
 const a = 5
 const b = 10
 const sum = `Сумма a и b равна ${a+b}`
 console.log(sum)
+
+
+
+const posX = 50
+const posY = -100
+const posZ = 1.55
+const point = (posX + posY + posZ) * 2 / posX
+console.log(point)
+
+console.log('Слово' / 100)
+console.log(100/0)
+console.log(-100/0)
+
+console.log(9900000000000091n + 1n)
+console.log(9900000000000091n + 2n)
+
+const ages = 20
+const isChilde = ages < 18
+console.log(isChilde)
