2b5fa958e2a6bb08799602720ec2fe022ac91781
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-03 15:10:09 +0500

    Loops in JavaScript  while, do...while, and for. Breaking a loop with break

---
+++ js/js7.js
@@ -0,0 +1,63 @@
+// let count = 0
+// while (count < 100) {
+//     console.log(count)
+//     count++
+// }
+
+// let countt = 0
+// do {
+//     console.log(countt)
+//     countt++
+// }while (countt < 10)
+// let count = 0
+// while(count <10){
+//     console.log(count)
+//     count++
+// }
+
+// for(let i=0; i<4; i++){
+//         alert(i)
+// }
+
+// let countt = 1
+// while(countt < 20){
+//     console.log(countt)
+//     countt++
+// }
+
+// let counttt = 10
+// while(counttt < 1){
+//     console.log(counttt)
+//     counttt--
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// let count = 0;
+// while (count <= 100) {
+//     if (count > 15) {
+//         break;
+//     }
+//     console.log(count);
+//     count++;
+// }
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
