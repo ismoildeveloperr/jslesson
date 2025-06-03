47bd4a589c2d793189a7c7c9793cf06b54ad3064
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-03 16:39:11 +0500

    Example for while do while break

--- js/js7.js
+++ js/js7.js
@@ -56,8 +56,175 @@
 //     count++;
 // }
 
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// for (let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// let count = 2;
+// while(count <=10){
+//     counsole.log(count);
+//     count +=2;
+// }
+
+// let i = 1;
+
+// do {
+//   console.log("Я учу JavaScript");
+//   i++;
+// } while (i <= 3);
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 === 0) {
+//     console.log("Первое число, делящееся на 7:", i);
+//     break;
+//   }
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`2 x ${i} = ${2 * i}`);
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i * i);
+}
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+}
+console.log("Сумма:", sum);
+
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+let i = 10;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+let j = 2;
+while (j <= 20) {
+  console.log(j);
+  j += 2;
+}
+
+let sum2 = 0;
+let k = 1;
+while (k <= 50) {
+  sum2 += k;
+  k++;
+}
+console.log("Сумма от 1 до 50:", sum2);
+
+let x = 101;
+while (true) {
+  if (x % 13 === 0) {
+    console.log("Число:", x);
+    break;
+  }
+  x++;
+}
+
+/*
+let number;
+while (true) {
+  number = Number(prompt("Введите число: "));
+  if (number < 0) break;
+}
+*/
+
+/*
+let name;
+do {
+  name = prompt("Введите имя:");
+} while (!name);
+console.log("Привет,", name);
+*/
+
+/*
+let age;
+do {
+  age = Number(prompt("Введите возраст:"));
+} while (age <= 0);
+*/
+
+/*
+let answer;
+do {
+  answer = prompt("Хотите продолжить?");
+} while (answer !== "нет");
+*/
+
+/*
+const secret = 7;
+let guess;
+do {
+  guess = Number(prompt("Угадай число от 1 до 10:"));
+} while (guess !== secret);
+console.log("Угадал!");
+*/
+
+/*
+let total = 0;
+let input;
+do {
+  input = Number(prompt("Введите число (0 — стоп):"));
+  total += input;
+} while (input !== 0);
+console.log("Сумма:", total);
+*/
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 17 === 0) {
+    console.log("Первое число на 17:", i);
+    break;
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i === 42) {
+    console.log("Остановились на:", i);
+    break;
+  }
+  console.log(i);
+}
+
+/*
+while (true) {
+  let num = Number(prompt("Введите число:"));
+  if (num < 0) break;
+}
+*/
+
+
 for (let i = 1; i <= 50; i++) {
-    if (i % 5 === 0) {
-        console.log(i);
-    }
+  if (i % 19 === 0) {
+    console.log("Нашли число:", i);
+    break;
+  }
+}
+
+
+let total2 = 0;
+for (let i = 1; i <= 100; i++) {
+  total2 += i;
+  if (total2 > 100) {
+    console.log("Сумма превысила 100 на числе:", i);
+    break;
+  }
 }
