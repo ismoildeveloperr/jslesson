45591438ac52ab54d8ff609a81dda51915d35281
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-03 09:37:32 +0500

    Comparison operator and Self-assessment test

--- js/js4.js
+++ js/js4.js
@@ -24,3 +24,47 @@ let message = 'Привет'
 message += ', друг!'
 message +=' Как дела?'
 console.log(message)
+
+let countt = 10
+countt --
+countt --
+countt -- 
+console.log(countt)
+
+let counttt = 20
+counttt ++
+counttt ++
+console.log(counttt)
+
+let counts = 25
+let newCounts = ++  counts
+console.log(newCounts)
+console.log(counts)
+
+console.log(2=='2')
+console.log(3=='3')
+console.log(3==='3')
+
+console.log(5 !== 10)
+
+
+console.log('1' + 1);
+console.log(Boolean(''))
+
+console.log(typeof (100 / 'Слово'))
+console.log(typeof null)
+
+const hellow="Привет "
+const menya="Меня "
+const zovut="зовут "
+const namee="Ismoil "
+const mne21let="мне 21 лет."
+console.log(hellow + menya + zovut + namee + mne21let)
+
+
+const age = 16;
+const isChilde = age < 18;
+console.log(isChilde)
+
+console.log("5" + 10); 
+console.log(10 + "5"); 
