b846bcb6a2f7b5cf98845f6796a7e4e5f2d72792
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-10 16:55:46 +0500

    lesson 9 object

--- js/jsday3.js
+++ js/jsday3.js
@@ -119,9 +119,146 @@
 //   getSecretMessage('')
 // )
 
-function greet(name){
- if (!name) return 'Я тебя не приветствую';
-  return `Я тебя приветствую, ${name}`;
-}
-console.log(greet('ismoil'))
-console.log(greet())
+// function greet(name){
+//  if (!name) return 'Я тебя не приветствую';
+//   return `Я тебя приветствую, ${name}`;
+// }
+// console.log(greet('ismoil'))
+// console.log(greet())
+
+// function logMessage(){
+//   console.log('Привет!');
+// }
+// logMessage();
+
+// function logMessage(){
+//   console.log('Пока!');
+// }
+
+// function logAll(){
+//   console.log(arguments)
+// }
+// logAll('Привет', 'Пока', 123, true, null, undefined, {name: 'Ismoil'}, [1,2,3]);
+// logMessage();
+
+// function logMessage(){
+//   console.log('Привет!');
+// }
+// logMessage();
+// const logMessage = function(){
+//   console.log('Пока!');
+// }
+
+// const logHello = () => {
+//   console.log('Привет!');
+// }
+// logHello();
+
+// const sum = (a, b) => a + b
+// console.log(sum(10, 20));
+// function fn1(){
+//   return 'Я функция fn1';
+// }
+// const fn2 = function(){
+//   return 'Я функция fn2';
+// }
+// const fn3 = () => {
+//   return 'Я функция fn3';
+// }
+// console.log(fn1());
+// console.log(fn2());
+// console.log(fn3());
+
+// const kvadrat = (a) => a * a;
+// console.log(kvadrat(5));
+
+// const rostkunja = (a,b) => (2 * (a + b))
+// console.log(rostkunja(5,4))
+
+// const firstEmptyObject = {};
+// const secondEmptyObject = new Object();
+
+// const user = {
+//   login: 'Ismoil',
+//   password: '123456',
+//   registrationdate: '2023-10-01',
+//   status: '1',
+//   age: 18,
+//   job: {
+//     title: 'Frontend Developer',
+//     salary: 1000,
+//     company: {
+//       name: 'IT Company',
+//       address: '123 Main St',
+//       city: 'Tajikistan',
+//       state: 'Dushanbe',  
+//       country: 'khujand',
+//       zip: '734000',
+//     }
+//   },
+//   sayHi:()=> console.log('Привет, я пользователь!'),
+// }
+
+// console.log(user.job.company.city);
+// console.log(user['age'])
+// console.log(user.job.salary)
+// console.log(user.job.company.zip)
+// user.sayHi();
+
+// const user = {}
+// user.name = 'Ismoil';
+// user['is developer'] = true;
+// console.log(user);
+// user.name = 'Ismoil Ismoilov';
+// user['is developer'] = false;
+// user.age = 18;
+// console.log(user);
+// delete user.age;
+// console.log(user);
+
+// const name = 'Ismoil';
+// const age = 21;
+
+// const user = {
+//   name: name,
+//   age: age,
+// };
+// console.log(user);
+
+
+// const name = 'Ismoil';
+// const age = 21;
+
+// const user = {
+//   name,
+//   age,
+// };
+// console.log(user);
+
+// const propName = prompt('Введите имя свойства объекта');
+// const propValue = prompt(`Введите значение свойства объекта "${propName}"`);
+// const user = {
+//   [propName]: propValue,
+// };
+// console.log(user)
+
+// const user = {
+//   name: 'Ismoil',
+//   age: 21,
+//   isDeveloper: true,
+// };
+// for(const key in user) {
+//   console.log(`Ключ: ${key}, Значение: ${user[key]}`);
+// }
+
+// const nums={
+//   'name':'ismoil',
+//   2:'Two',
+//   3:'Three',
+//   1:'One',
+//   age:20
+// }
+
+// for(const num in nums){
+//   console.log(nums[num])
+// }
